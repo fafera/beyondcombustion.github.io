c32b7949c69959d1ab09312004e54b379e121b1a
fafera <fafa.capellari@gmail.com>
2022-06-11 17:53:12 -0300

    default

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 //const fs = require('fs');
 //const data = fs.readFileSync('vaporizers.txt', 'utf8');
-var source = window.open('https://github.com/fafera/The-Consensus/blob/https://fafacapellari.com/source/vaporizers.txt');
-var xmlhttp = new XMLHttpRequest();
-var txt = '';
-xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
-        txt = xmlhttp.responseText;
-    }
-};
-xmlhttp.open("GET","vaporizers.txt",true);
-xmlhttp.send();
-
-window.onload = function() {
-    var splitedData = txt.split('\n');
-    const vapeList = [];
-    var i = 0;
-    splitedData.forEach(element => {
-        vapeList[i++] = {
-            title: getTitle(element),
-            categories: getCategories(element),
+let vapeList = [];
+let table = '';
+document.onreadystatechange = function() {
+    if(document.readyState === 'interactive') {
+        var xmlhttp = new XMLHttpRequest();
+        var txt = '';
+        xmlhttp.onreadystatechange = function(){
+            if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+                generateTable(xmlhttp.responseText);
+            }
         };
-    });
-    //console.log(vapeList);
+        xmlhttp.open("GET","vaporizers.txt",true);
+        xmlhttp.send();        
+    }
+    function generateTable(txt) {    
+        var splitedData = txt.split('\n');
+        var i = 0;
+        splitedData.forEach(element => {
+            vapeList[i++] = {
+                title: getTitle(element),
+                categories: getCategories(element),
+                price: getPrice(element)
+            };
+        });
+        mountHTMLObject();
+    }
+    function mountHTMLObject() {
+        vapeList.forEach(element => {
+            if(element.title) {
+                table += "<tr>" +
+                        "<td>"+element.title+"</td>" +
+                        "<td>"+element.categories+"</td>" +
+                        "<td>"+element.price+"</td"+
+                    "</tr>";
+                }
+        });
+    }
     function getTitle(element) {
         var title = element.substring(
             element.indexOf("-") +2, 
@@ -48,27 +63,30 @@ window.onload = function() {
             text.lastIndexOf("]")
         );
     }
+    function getNumericValue(text) {
+        text = text.substring(text.indexOf("$") +1);
+        for(var i =0; i<=text.length; i++) {
+            if(isNaN(text.charAt(i)) && text.charAt(i) != '-') {
+                return trimValue(text.substring(0,i));
+            }
+        }
+    }
+    function trimValue(text) {
+        if(text.charAt(text.length-1) == '-') {
+            return text.substring(0, text.length-1);
+        }
+        return text;
+    }
     function getCategories(text) {
         var categories = breakLastBrackets(text);
         return categories.split('/');
     }
-    function generateTable() {
-        var table = '';
-        vapeList.forEach(element => {
-            if(element.title) {
-                table += "<tr>" +
-                        "<td>"+element.title+"</td>" +
-                        "<td>"+element.categories+"</td>" +
-                    "</tr>";
-                }
-        });
-        return table;
+    function getPrice(text) {
+        return getNumericValue(text);
     }
-    var table = generateTable();
+}
+window.onload = function() {
     $("#vape-table").append(table);
-    //var htmlTable = document.getElementById('vape-table');
-    //htmlTable.append(table);
-    //htmlTable.outerHTML = table;
     $("#vape-table").fancyTable({
         sortColumn:0,
         pagination: true,
